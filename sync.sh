717beca910ffa76b9bde249787373f990a19cb94
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2021-11-22 13:04:57 -0600

    missing fi

--- sync.sh
+++ sync.sh
@@ -5,9 +5,11 @@ BASE_CMD="s3 sync s3://$FROM_BUCKET$FROM_KEY s3://$TO_BUCKET$TO_KEY $PARAMS"
 sleep 20
 
 echo "$(date) - Start"
+# https://stackoverflow.com/questions/39296472/how-to-check-if-an-environment-variable-exists-and-get-its-value
 if [[ -z "${AWS_S3_ENDPOINT}"]]; then
     aws --endpoint-url $AWS_S3_ENDPOINT ${BASE_CMD}
 else
     aws ${BASE_CMD}
+fi
 
 echo "$(date) End"
