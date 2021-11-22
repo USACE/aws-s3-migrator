a95e18c6fc6812dc7d3c6cebcb6f67ded94ed6c8
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2021-11-22 17:53:32 -0600

    Fix Sync

--- sync.sh
+++ sync.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-BASE_CMD="s3 sync s3://$FROM_BUCKET$FROM_KEY s3://$TO_BUCKET$TO_KEY $PARAMS"
-
 sleep 20
 
 echo "$(date) - Start"
 # https://stackoverflow.com/questions/39296472/how-to-check-if-an-environment-variable-exists-and-get-its-value
 if [[ -z "${AWS_S3_ENDPOINT}" ]]; then
-    "aws ${BASE_CMD}"
+    aws s3 sync "s3://${FROM_BUCKET}${FROM_KEY}" "s3://${TO_BUCKET}${TO_KEY}" ${PARAMS}
 else
-    "aws --endpoint-url $AWS_S3_ENDPOINT ${BASE_CMD}"
+    aws s3 sync --endpoint-url ${AWS_S3_ENDPOINT} "s3://${FROM_BUCKET}${FROM_KEY}" "s3://${TO_BUCKET}${TO_KEY}" ${PARAMS}
 fi
-
 echo "$(date) End"
