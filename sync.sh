0f1edcdc10416e6a1484c2e05e756c976bee6412
Brett Palmberg <29417036+brettpalmberg@users.noreply.github.com>
2021-11-22 12:46:12 -0600

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+BASE_CMD="s3 sync s3://$FROM_BUCKET$FROM_KEY s3://$TO_BUCKET$TO_KEY $PARAMS"
+
 sleep 20
 
 echo "$(date) - Start"
-
-aws --endpoint-url $AWS_S3_ENDPOINT s3 sync s3://$FROM_BUCKET$FROM_KEY s3://$TO_BUCKET$TO_KEY $PARAMS
+if [[ -z "${AWS_S3_ENDPOINT}"]]; then
+    aws --endpoint-url $AWS_S3_ENDPOINT ${BASE_CMD}
+else
+    aws ${BASE_CMD}
 
 echo "$(date) End"
